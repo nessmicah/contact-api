fd8282d9d635693e6efe121b765b444b54a87842
nessmicah <51240071+nessmicah@users.noreply.github.com>
2019-05-31 16:10:06 -0400

    Update Globals.cs

--- src/Globals.cs
+++ src/Globals.cs
@@ -2,5 +2,6 @@ namespace ContactApi
 {
   public static class Globals 
   {
-  public static string DbConnectionString { get; set; }  }
+    public static string DbConnectionString { get; set; }  
+  }
 }
